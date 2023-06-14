d07ff4cc350530481d1252bc21d6bd88ca4f94d3
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-06-14 10:30:45 -0700

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function to save to json"""
+
+
+def save_to_json_file(my_obj, filename):
+    """ This function saves obj to file in json format"""
+    obj_string = json.dumps(my_obj)
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(obj_string)

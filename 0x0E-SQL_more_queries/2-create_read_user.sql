b85fd08b817c0704f46ab02a627c9ab1eefe67a7
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-07-20 02:51:53 -0700

    task 2 why why why

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
---creates database
+--  creates the database hbtn_0d_2
+-- should have only SELECT privilege in the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- creates a user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grants SELECT privileges to a user
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;

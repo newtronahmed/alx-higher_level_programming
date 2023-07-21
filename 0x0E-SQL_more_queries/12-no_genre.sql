22581ef2125411547861267151c5b223df0128a0
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-07-20 21:53:15 -0700

    task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,4 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_shows_genres ON tv_shows_genres.show_id = tv_shows.id 
-WHERE tv_shows_genres.show_id IS NULL ORDER BY
-tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
+WHERE tv_show_genres.show_id IS NULL ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;

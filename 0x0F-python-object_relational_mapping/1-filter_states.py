c9bd4bf7269ed5c15c80975cea3dd2838ca02d2c
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-19 08:31:41 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """lists all states with a name starting with N"""
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
 
-db=MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
-        password=sys.argv[2],db=sys.argv[3])
-c=db.cursor()
-c.execute("SELECT * FROM states WHERE states.name
-        LIKE N% ORDER BY states.name;")
-for row in c.fetchall():
-    print(row)
+    import MySQLdb
+    import sys
+
+    db=MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
+            password=sys.argv[2],db=sys.argv[3])
+    c=db.cursor()
+    c.execute("""SELECT * FROM states WHERE states.name
+            LIKE BINARY 'N%' ORDER BY states.name""")
+    for row in c.fetchall():
+        print(row)

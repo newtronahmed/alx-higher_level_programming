afcb47c1bf5b73a690ec6e270fe25a067d5e4ead
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-19 11:16:08 -0700

    task model_state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" contains the class definition of a State"""
+
+if __name__ == "__main__":
+    
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import Column, Integer, String
+
+    Base = declarative_base()
+
+    class State(Base):
+        """ State model"""
+
+        __tablename__ = "states"
+
+        id = Column(Integer, primary_key=True, nullable=False, unique=True)
+        name = Column(String(128), nullable=False)

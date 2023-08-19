df83ed12c6cf91081c5337f41481e8358cdacf10
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-19 08:00:54 -0700

    spacious

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,14 +2,14 @@
 """displays all values in the states table of hbtn_0e_0_usa
 where name matches the argument."""
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     import MySQLdb
     import sys
 
-    db=MySQLdb.connect(host="localhost",port=3306,
-            user=sys.argv[1],password=sys.argv[2],db=sys.argv[3])
-    c=db.cursor()
+    db = MySQLdb.connect(host="localhost", port=3306,
+            user=sys.argv[1], password=sys.argv[2],db = sys.argv[3])
+    c = db.cursor()
 
     c.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}'\
             ORDER BY states.id".format(sys.argv[4]))

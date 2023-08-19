7037270829bdf1617d45da21e7fef05823219987
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-19 08:16:54 -0700

    pycodestyle done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db=MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
-            password=sys.argv[2],database=sys.argv[3])
-    q=db.cursor()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    q = db.cursor()
     q.execute("SELECT * FROM states ORDER BY states.id;")
     for each in q.fetchall():
         print(each)

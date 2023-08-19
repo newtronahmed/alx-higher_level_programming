498ae553df9918e8b6df4c64e20144b9e7946706
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-08-19 10:47:37 -0700

    done 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-""" lists all cities from the databas"""
+"""lists all cities of that state,"""
 
 if __name__ == "__main__":
 
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-            user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
 
-    c.execute("SELECT * FROM cities LEFT JOIN states ON cities.state_id=states.id\
-            ORDER BY cities.id ASC")
+    c.execute("SELECT cities.name FROM cities LEFT JOIN states\
+            ON cities.state_id = states.id\
+            WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = c.fetchall()
 
-    for row in c.fetchall()
-        print(row)
+    print(", ".join([row[0] for row in rows]))
+    c.close()
+    db.close()

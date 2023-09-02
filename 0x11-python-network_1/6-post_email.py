a0c561909dd161458ab0451c93699127415f56fc
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-09-02 07:08:11 -0700

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,9 +4,9 @@
     and finally displays the body of the response"""
 
 
-    if __name__ == "__main__":
-        import requests
-        import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-        r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-        print(r.text)
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)

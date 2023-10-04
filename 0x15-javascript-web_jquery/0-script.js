4adc6fc5f207b560369b6289d7d01b67471490a2
neutronahmed <bazubairu@st.ug.edu.gh>
2023-10-04 21:35:58 +0000

    task 0 to 7

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,3 @@
+// updates the text color of the <header> element to red (#FF0000
+const colorHeader = document.querySelector('header');
+colorHeader.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,3 @@
+//updates the text color of the <header> element to red (#FF0000)
+// when the user clicks on the tag DIV#red_header:
+$('DIV#red_header').click(function () {})
